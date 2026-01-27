fa7bbc9d8a9b703200cfceda29306462574f0048
DianaaRotaru <55446337+DianaaRotaru@users.noreply.github.com>
2026-01-27 20:27:05 +0100

    Remove FID-A dependency and all related files
    Deleted the entire code/dependencies/FID-A directory, including documentation, example data, input/output modules, processing tools, simulation tools, and associated scripts. This removes the FID-A package and all its components from the project.

--- code/dependencies/FID-A/processingTools/MRSI/helpers/checkSpatialFT.m
+++
@@ -1,5 +0,0 @@
-function checkSpatialFT(MRSIStruct) 
-    if(~getFlags(MRSIStruct, 'spatialFT'))
-        error('Please fourier transform along the spatial dimension')
-    end
-end
--- code/dependencies/FID-A/processingTools/MRSI/helpers/checkSpectralFT.m
+++
@@ -1,5 +0,0 @@
-function checkSpectralFT(MRSIStruct) 
-    if(~getFlags(MRSIStruct, 'spectralFT'))
-        error('Please Fourier transform along the spectral dimension')
-    end
-end
